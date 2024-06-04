7b6a860847db12b0f28743e30180191b6cd180d8
Brayden <braydenwu@cug.edu.cn>
2024-06-04 15:44:34 +0800

    主页面v1.0 今日收支明细绘制、收支页面与布局绘制

---
+++ MiDemo/app/src/main/java/com/example/midemo/RecordActivity.java
@@ -0,0 +1,13 @@
+package com.example.midemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+    }
+}
--- MiDemo/app/src/main/java/com/example/midemo/dao/UserDAO.java
+++ MiDemo/app/src/main/java/com/example/midemo/dao/UserDAO.java
@@ -1,56 +1,57 @@
 package com.example.midemo.dao;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
+import android.content.ContentValues; // 导入ContentValues类，用于存储一行数据的键值对
+import android.content.Context; // 导入Context类，用于获取上下文环境
+import android.database.Cursor; // 导入Cursor类，用于迭代查询结果
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 import com.example.midemo.utils.DatabaseHelper;
 
+// 定义UserDAO类，用于数据库操作
 public class UserDAO {
-    private SQLiteDatabase database;
-    private DatabaseHelper dbHelper;
+    private SQLiteDatabase database; // SQLiteDatabase对象，用于数据库操作
+    private DatabaseHelper dbHelper; // DatabaseHelper对象，用于创建和管理数据库
 
-    public UserDAO(Context context) {
-        dbHelper = new DatabaseHelper(context);
+    public UserDAO(Context context) { // UserDAO构造函数，接收Context对象
+        dbHelper = new DatabaseHelper(context); // 初始化DatabaseHelper对象
     }
 
     public void open() throws SQLException {
-        database = dbHelper.getWritableDatabase();
+        database = dbHelper.getWritableDatabase(); // 获取可写的数据库实例
         // 打印数据库文件路径
-        Log.d("Database Path", database.getPath());
+        Log.d("Database Path", database.getPath()); // 记录数据库文件路径
     }
 
     public void close() {
         dbHelper.close();
     }
 
-    public void addUser(String username, String password) {
-        ContentValues values = new ContentValues();
-        values.put(DatabaseHelper.COLUMN_USERNAME, username);
-        values.put(DatabaseHelper.COLUMN_PASSWORD, password);
-        database.insert(DatabaseHelper.TABLE_USER, null, values);
+    public void addUser(String username, String password) { // 添加用户方法，接收用户名和密码
+        ContentValues values = new ContentValues(); // 创建ContentValues对象
+        values.put(DatabaseHelper.COLUMN_USERNAME, username); // 将用户名放入ContentValues
+        values.put(DatabaseHelper.COLUMN_PASSWORD, password); // 将密码放入ContentValues
+        database.insert(DatabaseHelper.TABLE_USER, null, values); // 插入新行到用户表中
     }
 
+    // 检查用户方法，接收用户名和密码
     public boolean checkUser(String username, String password) {
         String[] columns = {
                 DatabaseHelper.COLUMN_ID
         };
-        String selection = DatabaseHelper.COLUMN_USERNAME + " = ?" + " AND " + DatabaseHelper.COLUMN_PASSWORD + " = ?";
-        String[] selectionArgs = {username, password};
+        String selection = DatabaseHelper.COLUMN_USERNAME + " = ?" + " AND " + DatabaseHelper.COLUMN_PASSWORD + " = ?"; // 查询条件
+        String[] selectionArgs = {username, password}; // 查询条件的参数
 
-        Cursor cursor = database.query(DatabaseHelper.TABLE_USER,
-                columns,
-                selection,
-                selectionArgs,
-                null,
-                null,
-                null);
+        Cursor cursor = database.query(DatabaseHelper.TABLE_USER, // 执行查询操作
+                columns, // 要查询的列
+                selection, // 查询条件
+                selectionArgs, // 查询条件的参数
+                null, // 不进行分组
+                null, // 不进行筛选
+                null); // 不进行排序
 
-        int cursorCount = cursor.getCount();
+        int cursorCount = cursor.getCount(); // 获取查询结果的行数
         cursor.close();
-
-        return cursorCount > 0;
+        return cursorCount > 0; // 如果查询结果行数大于0，则表示用户存在
     }
 }
